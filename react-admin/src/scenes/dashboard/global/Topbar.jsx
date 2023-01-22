03db2186a65c61b5394801ad51e44d9baa6f52fb
Zeta912 <ag.sebastian912@gmail.com>
2023-01-22 19:14:11 -0300

    Update to 22/01/23 7:14 PM

--- react-admin/src/scenes/dashboard/global/Topbar.jsx
+++ react-admin/src/scenes/dashboard/global/Topbar.jsx
@@ -1,4 +1,17 @@
+import { Box, IconButton, useTheme} from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext } from "../../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsModeOutlinedIcon from "@mui/icons-material/NotificationsModeOutlined";
+import SettingsModeOutlinedIcon from "@mui/icons-material/SettingsModeOutlined";
+import PersonModeOutlinedIcon from "@mui/icons-material/PersonModeOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
 const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     return <div>Topbar</div>
 }
 
