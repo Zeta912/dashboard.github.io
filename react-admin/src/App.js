cc15a7cc11cd37b87a4c55c3257a864a209b4e90
Zeta912 <ag.sebastian912@gmail.com>
2023-01-24 23:06:23 -0300

    Update to 24/01/23 11:06 PM

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Topbar from "./scenes/dashboard/global/Topbar"
+import {Routes, Route} from "react-router-dom";
+import Topbar from "./scenes/dashboard/global/Topbar";
+import Sidebar from "./"
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Dashboard from "./scenes/dashboard";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+
 
 function App() {
   const [theme, colorMode] = useMode ();
@@ -11,6 +24,17 @@ function App() {
     <div className="app">
       <main className="context">
         <Topbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
       </main>
     </div>
     </ThemeProvider>
